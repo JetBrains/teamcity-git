ea75aa999fabc2476ac74e0d6e876a6551a344a4
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-06-01 13:25:05 +0400

    take into account SubmodulesCheckoutPolicy, for SubmodulesCheckoutPolicy.NON_RECURSIVE_CHECKOUT - checkout only first level submodules, and for SubmodulesCheckoutPolicy.CHECKOUT - checkout submodules recursevly
    take into account property "teamcity.git.recursive.submodules", if it set to false - checkout only first level submodules, if set to true or not set - checkout submodules recursevly
    
    use SubmodulesCheckoutPolicy instead of boolean flag on server to check if SubmoduleAwareTreeIterator should retrieve found submodules

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/submodules/DirectSubmoduleAwareTreeIterator.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/submodules/DirectSubmoduleAwareTreeIterator.java
@@ -16,6 +16,7 @@
 
 package jetbrains.buildServer.buildTriggers.vcs.git.submodules;
 
+import jetbrains.buildServer.buildTriggers.vcs.git.SubmodulesCheckoutPolicy;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 
@@ -30,11 +31,15 @@ public class DirectSubmoduleAwareTreeIterator extends SubmoduleAwareTreeIterator
    *
    * @param wrappedIterator   the wrapped iterator
    * @param submoduleResolver the resolver for submodules
+   * @param repositoryUrl     the url of the repository of this iterator
+   * @param pathFromRoot      the path from the root of main repository to the entry of this repository
+   * @param submodulesPolicy  should created iterator checkout submodules
    * @throws IOException in case of submodule processing problem
    */
   public DirectSubmoduleAwareTreeIterator(AbstractTreeIterator wrappedIterator, SubmoduleResolver submoduleResolver,
-                                          String repositoryUrl, String pathFromRoot) throws IOException {
-    super(wrappedIterator, submoduleResolver, repositoryUrl, pathFromRoot);
+                                          String repositoryUrl, String pathFromRoot,
+                                          SubmodulesCheckoutPolicy submodulesPolicy) throws IOException {
+    super(wrappedIterator, submoduleResolver, repositoryUrl, pathFromRoot, submodulesPolicy);
   }
 
   /**
@@ -43,14 +48,18 @@ public class DirectSubmoduleAwareTreeIterator extends SubmoduleAwareTreeIterator
    * @param parent            the parent iterator
    * @param wrappedIterator   the wrapped iterator
    * @param submoduleResolver the resolver for submodules
+   * @param repositoryUrl     the url of the repository of this iterator
+   * @param pathFromRoot      the path from the root of main repository to the entry of this repository
+   * @param submodulesPolicy  should created iterator checkout submodules
    * @throws IOException in case of submodule processing problem
    */
   public DirectSubmoduleAwareTreeIterator(SubmoduleAwareTreeIterator parent,
                                           AbstractTreeIterator wrappedIterator,
                                           SubmoduleResolver submoduleResolver,
                                           String repositoryUrl,
-                                          String pathFromRoot) throws IOException {
-    super(parent, wrappedIterator, submoduleResolver, repositoryUrl, pathFromRoot);
+                                          String pathFromRoot,
+                                          SubmodulesCheckoutPolicy submodulesPolicy) throws IOException {
+    super(parent, wrappedIterator, submoduleResolver, repositoryUrl, pathFromRoot, submodulesPolicy);
   }
 
   /**
