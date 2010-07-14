ada63f06a0651d09886dcf547eb1097c8ed92a9a
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-07-14 20:49:49 +0400

    fix compilation

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/GitPathResolverImpl.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/GitPathResolverImpl.java
@@ -17,52 +17,17 @@
 package jetbrains.buildServer.buildTriggers.vcs.git.agent;
 
 import jetbrains.buildServer.agent.BuildAgentConfiguration;
-import jetbrains.buildServer.agent.parameters.AgentParameterResolverFactory;
-import jetbrains.buildServer.parameters.CompositeParametersProvider;
-import jetbrains.buildServer.parameters.ProcessingResult;
-import jetbrains.buildServer.parameters.ValueResolver;
-import jetbrains.buildServer.parameters.impl.CompositeParametersProviderImpl;
-import jetbrains.buildServer.parameters.impl.DynamicContextVariables;
-import jetbrains.buildServer.parameters.impl.MapParametersProviderImpl;
 import jetbrains.buildServer.vcs.VcsException;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author dmitry.neverov
  */
 public class GitPathResolverImpl implements GitPathResolver {
 
-  @NotNull private final AgentParameterResolverFactory myResolverFactory;
-
-  public GitPathResolverImpl(@NotNull final AgentParameterResolverFactory resolverFactory) {
-    myResolverFactory = resolverFactory;
+  public GitPathResolverImpl() {
   }
 
   public String resolveGitPath(final BuildAgentConfiguration agentConfiguration, String pathToResolve) throws VcsException {
-    ValueResolver valueResolver = getValueResolver(agentConfiguration);
-    ProcessingResult result = valueResolver.resolve(pathToResolve);
-    if (!result.isFullyResolved()) {
-      throw new VcsException("The value is not fully resolved: " + result.getResult());
-    }
-    return result.getResult();
-  }
-
-
-  /**
-   * Create a value resolver
-   *
-   * @param agentConfiguration the build agent configuration
-   * @return a created value resolver
-   */
-  private ValueResolver getValueResolver(final BuildAgentConfiguration agentConfiguration) {
-    final CompositeParametersProvider parametersProvider = new CompositeParametersProviderImpl();
-    parametersProvider
-      .appendParametersProvider(new MapParametersProviderImpl("agent parameters", agentConfiguration.getAgentParameters()));
-    parametersProvider
-      .appendParametersProvider(new MapParametersProviderImpl("agent custom parameters", agentConfiguration.getCustomProperties()));
-
-    final DynamicContextVariables variables = new DynamicContextVariables();
-
-    return myResolverFactory.createValueResolver(variables, parametersProvider);
+    return pathToResolve;
   }
 }
