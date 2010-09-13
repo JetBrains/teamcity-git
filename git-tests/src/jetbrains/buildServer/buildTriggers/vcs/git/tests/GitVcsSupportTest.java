b6459b180c85becfda4691904676c6b4f0da3881
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-09-13 21:20:02 +0400

    Fix TW-13330
    Bug was in Fetcher, it worked only if all parameters of VcsRoot sent
    to process input as string were smaller than 512 bytes (most of the
    cases) or size mod 512 = 0.

--- git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/GitVcsSupportTest.java
+++ git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/GitVcsSupportTest.java
@@ -849,4 +849,28 @@ public class GitVcsSupportTest extends PatchTestCase {
       assertEquals(GitUtils.normalizePath(d[0]), d[1]);
     }
   }
+
+
+  /**
+   * TW-13330
+   * Test reproduces bug in Fetcher code: Fetcher worked only if all parameters of VcsRoot
+   * sent to process input as string were smaller than 512 bytes (most of the cases) or size mod 512 = 0.
+   */
+  @Test
+  public void test_long_input_for_fetcher_process() throws IOException, VcsException {
+    System.setProperty("teamcity.git.fetch.separate.process", "true");
+    GitVcsSupport support = getSupport();
+    VcsRootImpl root = (VcsRootImpl) getRoot("version-test");
+    root.addProperty("param",
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" +
+                     "bbbbbbbbbbbbbbbbbbbbb");//with such long param size of input for fetcher process is greater than 512 bytes
+    String version = support.getCurrentVersion(root);
+    assertEquals(VERSION_TEST_HEAD, version);
+  }
 }
