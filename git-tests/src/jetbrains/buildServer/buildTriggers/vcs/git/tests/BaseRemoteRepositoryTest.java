a14ab734b8cc3612f68d96393d7934aef150d9c4
Dmitry Neverov <dmitry.neverov@gmail.com>
2012-11-28 17:45:31 +0400

    Extract testConnection tests to separate class

---
+++ git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/BaseRemoteRepositoryTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.buildTriggers.vcs.git.tests;
+
+import jetbrains.buildServer.TempFiles;
+import jetbrains.buildServer.buildTriggers.vcs.git.GitUtils;
+import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static jetbrains.buildServer.buildTriggers.vcs.git.tests.GitTestUtil.copyRepository;
+import static jetbrains.buildServer.buildTriggers.vcs.git.tests.GitTestUtil.dataFile;
+
+public abstract class BaseRemoteRepositoryTest {
+
+  protected TempFiles myTempFiles;
+  private String[] myRepositories;
+  private Map<String, File> myRemoteRepositories;
+
+  protected BaseRemoteRepositoryTest(String... repositories) {
+    myRepositories = repositories;
+  }
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    myTempFiles = new TempFiles();
+    File tmp = myTempFiles.createTempDir();
+    myRemoteRepositories = new HashMap<String, File>();
+    for (String r : myRepositories) {
+      File remoteRepository = new File(tmp, r);
+      copyRepository(dataFile(r), remoteRepository);
+      myRemoteRepositories.put(r, remoteRepository);
+    }
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    myTempFiles.cleanup();
+  }
+
+  protected String getRemoteRepositoryUrl(@NotNull String remoteRepository) {
+    File remote = myRemoteRepositories.get(remoteRepository);
+    if (remote == null)
+      throw new IllegalArgumentException("No remote repository found: " + remoteRepository);
+    return GitUtils.toURL(remote);
+  }
+}
