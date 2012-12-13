7007e71a28bd931db824899d7b03b8f8f191ae44
Dmitry Neverov <dmitry.neverov@gmail.com>
2012-12-13 21:01:49 +0400

    Parse lines with StringUtil to take different line-endings into account

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/command/impl/ShowRefCommandImpl.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/command/impl/ShowRefCommandImpl.java
@@ -17,6 +17,7 @@
 package jetbrains.buildServer.buildTriggers.vcs.git.agent.command.impl;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.openapi.util.text.StringUtil;
 import jetbrains.buildServer.ExecResult;
 import jetbrains.buildServer.buildTriggers.vcs.git.agent.GitCommandLine;
 import jetbrains.buildServer.buildTriggers.vcs.git.agent.command.ShowRefCommand;
@@ -69,7 +70,9 @@ public class ShowRefCommandImpl implements ShowRefCommand {
 
   private Map<String, Ref> parse(String str) {
     Map<String, Ref> result = new HashMap<String, Ref>();
-    for (String line : str.split("\n")) {
+    for (String line : StringUtil.splitByLines(str)) {
+      if (line.length() < 41)
+        continue;//a valid line of show-ref output contains 40 symbols of hash + space + branch name
       String commit = line.substring(0, 40);
       String ref = line.substring(41, line.length());
       result.put(ref, new RefImpl(ref, commit));
