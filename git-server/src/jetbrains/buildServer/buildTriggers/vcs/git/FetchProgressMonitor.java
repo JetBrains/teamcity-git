f5c6c06567afea3a7b431fa6dd7041781433240a
Dmitry Neverov <dmitry.neverov@gmail.com>
2014-01-02 16:59:56 +0100

    TW-34176 fix line-endings

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/FetchProgressMonitor.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/FetchProgressMonitor.java
@@ -81,7 +81,6 @@ public class FetchProgressMonitor implements ProgressMonitor {
 
   private void display(final int cmp) {
     final StringBuilder m = new StringBuilder();
-    m.append('\r');
     m.append(msg);
     m.append(": ");
     while (m.length() < 25)
