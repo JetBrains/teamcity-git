76a893a2d4ca7a75195e6dc6d57b565068a69abe
Dmitry Neverov <dmitry.neverov@gmail.com>
2015-04-10 20:54:04 +0200

    Trying to fix TW-38443
    Cherry-pick 8cd4feaca2d0d5746ea639c4f1c0758a1bf0ac23

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/GitLabelingSupport.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/GitLabelingSupport.java
@@ -23,13 +23,17 @@ import jetbrains.buildServer.vcs.VcsException;
 import jetbrains.buildServer.vcs.VcsRoot;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.PushConnection;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
 import org.eclipse.jgit.transport.Transport;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.Collections;
 
 import static com.intellij.openapi.util.text.StringUtil.isEmpty;
@@ -63,26 +67,31 @@ class GitLabelingSupport implements LabelingSupport {
     OperationContext context = myVcs.createContext(root, "labelling");
     GitVcsRoot gitRoot = context.getGitRoot();
     try {
+      long start = System.currentTimeMillis();
       Repository r = context.getRepository();
       String commitSHA = GitUtils.versionRevision(version);
       RevCommit commit = myCommitLoader.loadCommit(context, gitRoot, commitSHA);
       Git git = new Git(r);
-      git.tag().setTagger(gitRoot.getTagger(r))
+      Ref tagRef = git.tag().setTagger(gitRoot.getTagger(r))
         .setName(label)
         .setObjectId(commit)
         .call();
-      String tagRef = GitUtils.tagName(label);
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Tag created  " + label + "=" + version + " for " + gitRoot.debugInfo());
+      if (tagRef.getObjectId() == null || resolve(r, tagRef) == null) {
+        LOG.warn("Tag's " + tagRef.getName() + " objectId " + (tagRef.getObjectId() != null ? tagRef.getObjectId().name() + " " : "") + "cannot be resolved");
+      } else if (LOG.isDebugEnabled()) {
+        LOG.debug("Tag created  " + label + "=" + version + " for " + gitRoot.debugInfo() +
+                  " in " + (System.currentTimeMillis() - start) + "ms");
       }
+      long pushStart = System.currentTimeMillis();
       synchronized (myRepositoryManager.getWriteLock(gitRoot.getRepositoryDir())) {
         final Transport tn = myTransportFactory.createTransport(r, gitRoot.getRepositoryPushURL(), gitRoot.getAuthSettings());
         try {
           final PushConnection c = tn.openPush();
           try {
-            RemoteRefUpdate ru = new RemoteRefUpdate(r, tagRef, tagRef, false, null, null);
-            c.push(NullProgressMonitor.INSTANCE, Collections.singletonMap(tagRef, ru));
-            LOG.info("Tag  " + label + "=" + version + " pushed with status " + ru.getStatus() + " for " + gitRoot.debugInfo());
+            RemoteRefUpdate ru = new RemoteRefUpdate(r, tagRef.getName(), tagRef.getObjectId(), tagRef.getName(), false, null, null);
+            c.push(NullProgressMonitor.INSTANCE, Collections.singletonMap(tagRef.getName(), ru));
+            LOG.info("Tag  " + label + "=" + version + " was pushed with status " + ru.getStatus() + " for " + gitRoot.debugInfo() +
+                     " in " + (System.currentTimeMillis() - pushStart) + "ms");
             switch (ru.getStatus()) {
               case UP_TO_DATE:
               case OK:
@@ -105,6 +114,15 @@ class GitLabelingSupport implements LabelingSupport {
       throw context.wrapException(e);
     } finally {
       context.close();
+    }
+  }
+
+  @Nullable
+  private ObjectId resolve(final Repository r, final Ref tagRef) {
+    try {
+      return r.resolve(tagRef.getName());
+    } catch (IOException e) {
+      return null;
     }
   }
 }
