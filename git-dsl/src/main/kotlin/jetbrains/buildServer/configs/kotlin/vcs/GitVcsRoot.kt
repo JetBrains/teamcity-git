9a46ec494cbd891eac51806ee88d0bf2cc7f8eb1
Dmitry Neverov <dmitry.neverov@gmail.com>
2016-06-06 14:44:08 +0200

    Move to vcs package

--- git-dsl/src/main/kotlin/jetbrains/buildServer/configs/kotlin/vcs/GitVcsRoot.kt
+++ git-dsl/src/main/kotlin/jetbrains/buildServer/configs/kotlin/vcs/GitVcsRoot.kt
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jetbrains.buildServer.configs.kotlin.v0
+
+package jetbrains.buildServer.configs.kotlin.vcs
+
+import jetbrains.buildServer.configs.kotlin.v0.KVcsRoot
 
 open class GitVcsRoot : KVcsRoot {
 
