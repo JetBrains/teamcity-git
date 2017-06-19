00961fdd093f28e919fa71a60e6888354badbb03
Dmitry Neverov <dmitry.neverov@gmail.com>
2017-06-19 14:57:36 +0200

    Pick-up changed gc cron without server restart

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/CleanupRunner.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/CleanupRunner.java
@@ -21,15 +21,22 @@ import org.jetbrains.annotations.NotNull;
 import org.quartz.CronExpression;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class CleanupRunner implements Runnable {
+public class CleanupRunner {
 
   private final ServerPluginConfig myConfig;
   private final ScheduledExecutorService myExecutor;
   private final Cleanup myCleanup;
-  private volatile boolean myFirstRun = true;
+  private final AtomicReference<String> myCron = new AtomicReference<>();
+  private final AtomicReference<ScheduledFuture<?>> myCleanupFeature = new AtomicReference<>();
+  //lock is used to have only one scheduled clean-up task
+  private final ReentrantLock myLock = new ReentrantLock();
 
   public CleanupRunner(@NotNull final ExecutorServices executor,
                        @NotNull final ServerPluginConfig config,
@@ -37,29 +44,70 @@ public class CleanupRunner implements Runnable {
     myExecutor = executor.getNormalExecutorService();
     myConfig = config;
     myCleanup = cleanup;
-    myExecutor.submit(this);
+    myExecutor.scheduleAtFixedRate(this::updateSchedule, 10, 10, TimeUnit.MINUTES);
   }
 
-  public void run() {
-    if (myFirstRun) {
-      myFirstRun = false;
-    } else {
-      myCleanup.run();
+
+  /**
+   * Schedules a new cleanup task if cron expression was changed
+   */
+  private void updateSchedule() {
+    if (!myLock.tryLock())
+      return;
+    try {
+      CronExpression cron = myConfig.getCleanupCronExpression();
+      String cronStr = cron != null ? cron.getCronExpression() : null;
+      if (Objects.equals(myCron.get(), cronStr))
+        return;
+
+      ScheduledFuture<?> feature = myCleanupFeature.get();
+      if (feature != null) {
+        feature.cancel(false);
+      }
+
+      if (cron != null) {
+        schedule(cron);
+      } else {
+        myCleanupFeature.set(null);
+        myCron.set(null);
+      }
+    } finally {
+      myLock.unlock();
     }
-    schedule();
   }
 
-  private void schedule() {
-    CronExpression cron = myConfig.getCleanupCronExpression();
-    if (cron != null) {
-      Date now = new Date();
-      Date next = cron.getNextValidTimeAfter(now);
-      myExecutor.schedule(this, next.getTime() - now.getTime(), TimeUnit.MILLISECONDS);
-    } else {
-      //schedule ourselves to check if the cron expression specified
-      myExecutor.schedule(this, 10, TimeUnit.MINUTES);
-      //do not run cleanup next time, just schedule
-      myFirstRun = true;
+
+  /**
+   * Runs cleanup and schedules itself
+   */
+  private void runCleanup() {
+    myCleanup.run();
+
+    myLock.lock();
+    try {
+      CronExpression cron = myConfig.getCleanupCronExpression();
+      //schedule next clean-up task only if cron expression wasn't changed,
+      //changed cron handled by updateSchedule task
+      if (cron != null && Objects.equals(myCron.get(), cron.getCronExpression())) {
+        schedule(cron);
+      } else {
+        //reset future & cron to avoid ABA problem in updateSchedule(): cron is A,
+        //updateSchedule() remembers it, cron becomes B, runCleanup() doesn't schedule,
+        //cron is changed back to A, updateSchedule() also doesn't schedule
+        myCleanupFeature.set(null);
+        myCron.set(null);
+      }
+    } finally {
+      myLock.unlock();
     }
+  }
+
+
+  private void schedule(@NotNull CronExpression cron) {
+    Date now = new Date();
+    Date next = cron.getNextValidTimeAfter(now);
+    ScheduledFuture<?> future = myExecutor.schedule(this::runCleanup, next.getTime() - now.getTime(), TimeUnit.MILLISECONDS);
+    myCleanupFeature.set(future);
+    myCron.set(cron.getCronExpression());
   }
 }
