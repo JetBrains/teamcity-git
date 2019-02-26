154e2a06799f8eecf018fa5c3d3c6c5cf5f0ad82
linfar <andrey.titov@jetbrains.com>
2019-02-26 12:09:07 +0300

    Minor refactoring: extract SNISSLSocketFactory to a separate file

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/SNIHttpClientConnection.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/SNIHttpClientConnection.java
@@ -17,11 +17,9 @@
 package jetbrains.buildServer.buildTriggers.vcs.git;
 
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.text.StringUtil;
 import jetbrains.buildServer.util.ssl.SSLContextUtil;
 import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.*;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.conn.params.ConnRoutePNames;
@@ -44,7 +42,6 @@ import javax.net.ssl.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Method;
 import java.net.*;
 import java.net.ProtocolException;
 import java.security.KeyManagementException;
@@ -411,38 +408,4 @@ public class SNIHttpClientConnection implements HttpConnection {
   }
 
 
-  private class SNISSLSocketFactory extends SSLSocketFactory {
-    public SNISSLSocketFactory(final SSLContext sslContext) {
-      super(sslContext);
-    }
-
-    public SNISSLSocketFactory(final SSLContext sslContext, final X509HostnameVerifier hostnameVerifier) {
-      super(sslContext, hostnameVerifier);
-    }
-
-    @Override
-    public Socket connectSocket(final int connectTimeout,
-                                final Socket socket,
-                                final HttpHost host,
-                                final InetSocketAddress remoteAddress,
-                                final InetSocketAddress localAddress,
-                                final HttpContext context) throws IOException {
-      if (socket instanceof SSLSocket) {
-        enableSNI((SSLSocket)socket, host);
-      }
-      return super.connectSocket(connectTimeout, socket, host, remoteAddress, localAddress, context);
-    }
-
-    private void enableSNI(SSLSocket socket, final HttpHost host) {
-      String sniEnabled = System.getProperty("jsse.enableSNIExtension");
-      if (StringUtil.isEmpty(sniEnabled) || Boolean.parseBoolean(sniEnabled)) {
-        try {
-          Method method = socket.getClass().getDeclaredMethod("setHost", String.class);
-          method.invoke(socket, host.getHostName());
-        } catch (Exception e) {
-          LOG.info("Cannot enable SNI for host " + host.getHostName() + ", continue without SNI, error: " + e.toString());
-        }
-      }
-    }
-  }
 }
