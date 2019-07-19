d6a27ee1a0cad59eee303cbe850abf1d99ea413d
Mikhail Khorkov <mikhail.khorkov@jetbrains.com>
2019-07-19 13:04:36 +0700

    Do not unnecessary fetch in case we use Alternative strategy

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/UpdaterWithAlternates.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/UpdaterWithAlternates.java
@@ -66,7 +66,7 @@ public class UpdaterWithAlternates extends UpdaterWithMirror {
 
   @Override
   protected void ensureCommitLoaded(boolean fetchRequired) throws VcsException {
-    super.fetchFromOriginalRepository(fetchRequired);
+    checkHasRevisionOrThrow();
   }
 
 
