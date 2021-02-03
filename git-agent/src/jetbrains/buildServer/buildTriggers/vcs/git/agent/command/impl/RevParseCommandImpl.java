91579cd8b0d46c77df755c1f89998e6b2a654ce2
Victory Petrenko <vbedrosova@gmail.com>
2021-02-03 14:08:01 +0100

    supported passing parameters to rev-parse command

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/command/impl/RevParseCommandImpl.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/command/impl/RevParseCommandImpl.java
@@ -6,16 +6,29 @@ import jetbrains.buildServer.buildTriggers.vcs.git.agent.command.RevParseCommand
 import jetbrains.buildServer.vcs.VcsException;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RevParseCommandImpl extends BaseCommandImpl implements RevParseCommand {
     private String ref;
+    List<String> myParams = new ArrayList<>();
 
     public RevParseCommandImpl(@NotNull GitCommandLine myCmd) {
         super(myCmd);
     }
 
+    @NotNull
     @Override
     public RevParseCommand setRef(final String ref) {
         this.ref = ref;
+        return this;
+    }
+
+    @NotNull
+    @Override
+    public RevParseCommand setParams(final String... params) {
+        myParams.addAll(Arrays.asList(params));
         return this;
     }
 
@@ -23,6 +36,7 @@ public class RevParseCommandImpl extends BaseCommandImpl implements RevParseComm
     public String call() throws VcsException {
         GitCommandLine cmd = getCmd();
         cmd.addParameter("rev-parse");
+        cmd.addParameters(myParams);
         if (ref != null)
             cmd.addParameter(ref);
 
