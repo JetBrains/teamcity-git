4539cde166dbd9f050c5b16d5415e802bfb170d7
Victory Petrenko <vbedrosova@gmail.com>
2021-02-16 22:18:13 +0100

    add refs/heads/* branch spec by default
    (cherry picked from commit 83294ce6f4f16ee6ed94e56cf618647187881965)
    
    Mention that default branch also supports tags
    
    (cherry picked from commit 07a8b5b48ac29f21cd33c0cfd660ede0d69bcf22)
    
    moved class to the proper module
    
    (cherry picked from commit 21ccbbdef171dc6644b1a2711e655a8635651062)
    
    TW-67060, TW-69111, TW-63928: now when git VCS root is created from a repo URL, it's possible to specify default branch and branch spec
    
    (cherry picked from commit a2b9772a86ca76979a6509ce2f1bd598439e1373)

---
+++ git-server-tc/src/jetbrains/buildServer/buildTriggers/vcs/git/GitCustomizeVcsRootExtension.java
@@ -0,0 +1,48 @@
+package jetbrains.buildServer.buildTriggers.vcs.git;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import jetbrains.buildServer.controllers.admin.projects.setupFromUrl.SetupObjectFromResourceBean;
+import jetbrains.buildServer.controllers.admin.projects.setupFromUrl.SetupObjectFromResourcePageExtension;
+import jetbrains.buildServer.serverSide.discovery.VcsResourceDiscoveryExtension;
+import jetbrains.buildServer.web.openapi.PagePlaces;
+import jetbrains.buildServer.web.openapi.PlaceId;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.SimplePageExtension;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Extension for customizing git VCS root parameters when creating from repository URL.
+ * Currently it's possible to customize default branch and branch spec.
+ *
+ * @since 2021.1
+ */
+public class GitCustomizeVcsRootExtension extends SimplePageExtension implements GitServerExtension {
+
+  public GitCustomizeVcsRootExtension(@NotNull PagePlaces pagePlaces, @NotNull PluginDescriptor pluginDescriptor) {
+    super(pagePlaces, PlaceId.ADMIN_CUSTOMIZE_VCS_ROOT, GitServerExtension.class.getName(), pluginDescriptor.getPluginResourcesPath("gitCustomizeBranches.jsp"));
+    register();
+  }
+
+  @Override
+  public boolean isAvailable(@NotNull HttpServletRequest request) {
+    if (!super.isAvailable(request)) return false;
+
+    final SetupObjectFromResourceBean projectSetupBean = getSetupObjectFromResourceBean(request);
+    return projectSetupBean != null && Constants.VCS_NAME.equals(projectSetupBean.getDiscoveredResource().getParameters().get(VcsResourceDiscoveryExtension.VCS_NAME_PROP_KEY));
+  }
+
+  protected SetupObjectFromResourceBean getSetupObjectFromResourceBean(final HttpServletRequest request) {
+    return (SetupObjectFromResourceBean)request.getAttribute(SetupObjectFromResourcePageExtension.SETUP_OBJECT_FROM_RESOURCE_BEAN_KEY);
+  }
+
+  @Override
+  public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
+    super.fillModel(model, request);
+
+    final SetupObjectFromResourceBean projectSetupBean = getSetupObjectFromResourceBean(request);
+    if (projectSetupBean != null) {
+      projectSetupBean.getDiscoveredResource().getParameters().put(Constants.BRANCH_SPEC, "refs/heads/*");
+    }
+  }
+}
