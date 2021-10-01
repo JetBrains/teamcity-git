a4b29b4d2c68f1e902d8c9a7cdade48e6c95a0c6
pavel.sher <pavel.sher@jetbrains.com>
2021-10-01 15:16:25 +0200

    add forgotten class to the classpath

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/GitAgentSSHService.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/GitAgentSSHService.java
@@ -29,6 +29,7 @@ import jetbrains.buildServer.util.StringUtil;
 import jetbrains.buildServer.util.jsch.JSchConfigInitializer;
 import jetbrains.buildServer.vcs.VcsException;
 import org.apache.log4j.Logger;
+import org.bouncycastle.crypto.CipherParameters;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.git4idea.ssh.GitSSHHandler;
@@ -88,6 +89,7 @@ public class GitAgentSSHService extends GitSSHService {
       if (myScript == null || myScriptPath == null || !myScript.exists()) {
         ScriptGenerator generator = new ScriptGenerator(GitSSHHandler.GIT_SSH_PREFIX, JSchClient.class, getTempDir());
         generator.addClasses(JSch.class);
+        generator.addClasses(CipherParameters.class);
         generator.addClasses(JZlib.class);
         generator.addClasses(GitUtils.class);
         generator.addClasses(NotNull.class);
