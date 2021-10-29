30e84f43b7c86a72a090b8f01db8d83edd0c5235
vbedrosova <vbedrosova@gmail.com>
2021-10-29 13:28:11 +0200

    attempt to fix test

--- git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/CleanerTest.java
+++ git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/CleanerTest.java
@@ -31,6 +31,7 @@ import jetbrains.buildServer.vcs.VcsException;
 import jetbrains.buildServer.vcs.VcsRoot;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -109,6 +110,19 @@ public class CleanerTest extends BaseTestCase {
   }
 
 
+  private static void copy(@NotNull final File from, @NotNull final File to) throws IOException {
+    for (int i = 1; i <= 5; ++i) {
+      try {
+        FileUtil.copy(from, to);
+        break;
+      } catch (IOException e) {
+        if (i == 5) {
+          throw e;
+        }
+      }
+    }
+  }
+
   public void nonInplaceGc() throws Exception {
     myConfigBuilder.setRunNativeGC(true);
     myConfigBuilder.setRunInPlaceGc(false);
@@ -128,8 +142,8 @@ public class CleanerTest extends BaseTestCase {
     File pack = packs[0];
     File idx = new File(packDir, StringUtil.replace(pack.getName(), ".pack", ".idx"));
     for (int i = 10; i <= 60; i++) {
-      FileUtil.copy(pack, new File(packDir, "pack-" + i + "63fffad1c368b0a79f9a196ee098e303fc0c29.pack"));
-      FileUtil.copy(idx, new File(packDir, "pack-" + i + "63fffad1c368b0a79f9a196ee098e303fc0c29.idx"));
+      copy(pack, new File(packDir, "pack-" + i + "63fffad1c368b0a79f9a196ee098e303fc0c29.pack"));
+      copy(idx, new File(packDir, "pack-" + i + "63fffad1c368b0a79f9a196ee098e303fc0c29.idx"));
     }
     FileRepository db = (FileRepository) new RepositoryBuilder().setGitDir(repositoryDir).build();
     then(db.getObjectDatabase().getPacks().size() > 50).isTrue();
@@ -139,7 +153,6 @@ public class CleanerTest extends BaseTestCase {
     db = (FileRepository) new RepositoryBuilder().setGitDir(repositoryDir).build();
     then(db.getObjectDatabase().getPacks().size()).isEqualTo(1);
   }
-
 
   private void initCleanup() {
     myConfig = myConfigBuilder.build();
