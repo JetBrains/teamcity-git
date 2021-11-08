48cac0a05c7c793ff158eb9a2c31e817db32e13d
vbedrosova <vbedrosova@gmail.com>
2021-11-08 14:45:21 +0100

    TW-73873 correctly process empty username
    (cherry picked from commit 2620972ed1a1941a9bdcdda7a76711199fbafbf4)

--- git-common/src/jetbrains/buildServer/buildTriggers/vcs/git/command/credentials/CredentialsHelperConfig.java
+++ git-common/src/jetbrains/buildServer/buildTriggers/vcs/git/command/credentials/CredentialsHelperConfig.java
@@ -21,7 +21,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static jetbrains.buildServer.buildTriggers.vcs.git.command.credentials.CredentialsHelper.*;
 
@@ -29,7 +31,7 @@ public class CredentialsHelperConfig {
   private final List<Trinity<String, String, String>> myCredentials = new ArrayList<Trinity<String, String, String>>();
   private boolean myMatchAllUrls;
 
-  public void addCredentials(@NotNull String url, @NotNull String user, @NotNull String password) {
+  public void addCredentials(@NotNull String url, @Nullable String user, @NotNull String password) {
     myCredentials.add(Trinity.create(url, user, password));
   }
 
@@ -43,7 +45,9 @@ public class CredentialsHelperConfig {
     int i = 1;
     for (Trinity<String, String, String> credential : myCredentials) {
       result.put(credEnv(i, CRED_URL), credential.first);
-      result.put(credEnv(i, CRED_USER), credential.second);
+      if (StringUtil.isNotEmpty(credential.second)) {
+        result.put(credEnv(i, CRED_USER), credential.second);
+      }
       result.put(credEnv(i, CRED_PWD), credential.third);
     }
     if (myMatchAllUrls)
