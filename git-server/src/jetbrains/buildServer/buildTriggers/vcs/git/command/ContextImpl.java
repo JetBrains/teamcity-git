4bcc26995a004d4ae9b6d1b8a7e189491e4f1b24
vbedrosova <vbedrosova@gmail.com>
2022-01-19 15:10:55 +0100

    fixed typo in property name

--- git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/command/ContextImpl.java
+++ git-server/src/jetbrains/buildServer/buildTriggers/vcs/git/command/ContextImpl.java
@@ -167,6 +167,6 @@ public class ContextImpl implements Context {
 
   @Override
   public boolean isUseSshAskPass() {
-    return TeamCityProperties.getBooleanOrTrue("teamcity.git.useSshAskPas");
+    return TeamCityProperties.getBooleanOrTrue("teamcity.git.useSshAskPass");
   }
 }
