b028487d784b453f4e85dcd4095ae1cb7586c07a
Andre Rocha <andre.rocha@jetbrains.com>
2022-09-08 18:21:10 +0100

    TW-77411 Switch bundled plugins to use a persistent cache of settings

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/AgentStartupGitDetector.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/AgentStartupGitDetector.java
@@ -20,16 +20,16 @@ import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.openapi.util.SystemInfo;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import jetbrains.buildServer.ExecResult;
+import jetbrains.buildServer.ExtensionHolder;
 import jetbrains.buildServer.SimpleCommandLineProcessRunner;
-import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
-import jetbrains.buildServer.agent.AgentLifeCycleListener;
-import jetbrains.buildServer.agent.BuildAgent;
+import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.agent.config.AgentParametersSupplier;
 import jetbrains.buildServer.buildTriggers.vcs.git.Constants;
 import jetbrains.buildServer.buildTriggers.vcs.git.GitVersion;
-import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.util.FileUtil;
 import jetbrains.buildServer.vcs.VcsException;
 import org.apache.log4j.Logger;
@@ -39,7 +39,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * @author dmitry.neverov
  */
-public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
+public class AgentStartupGitDetector implements AgentParametersSupplier {
 
   private final static Logger LOG = Logger.getLogger(AgentStartupGitDetector.class);
 
@@ -48,21 +48,26 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
   private static final String[] WIN_PATHS = {"C:\\Program Files\\Git\\bin\\", "C:\\Program Files (x86)\\Git\\bin\\", "C:\\cygwin\\bin\\"};
   private static final String[] UNIX_PATHS = {"/usr/local/bin/", "/usr/bin/", "/opt/local/bin/", "/opt/bin/"};
   private static final String GIT_LFS_VERSION_PREFIX = "git-lfs/";
+  @NotNull private final BuildAgentConfiguration myBuildAgentConfiguration;
 
-  public AgentStartupGitDetector(@NotNull final EventDispatcher<AgentLifeCycleListener> dispatcher) {
-    dispatcher.addListener(this);
+  public AgentStartupGitDetector(@NotNull final ExtensionHolder extensionHolder, @NotNull final BuildAgentConfiguration buildAgentConfiguration) {
+    myBuildAgentConfiguration = buildAgentConfiguration;
+    extensionHolder.registerExtension(AgentParametersSupplier.class, getClass().getName(), this);
   }
+
   @Override
-  public void afterAgentConfigurationLoaded(@NotNull BuildAgent agent) {
-    String configuredGitPath = getConfiguredGitPath(agent);
+  public Map<String, String> getParameters() {
+    final Map<String, String> parameters = new HashMap<>();
+
+    String configuredGitPath = getConfiguredGitPath();
     if (configuredGitPath == null) {
       for (String path : getCandidatePaths()) {
         try {
           final GitVersion version = new AgentGitFacadeImpl(path).version().call();
-          agent.getConfiguration().addEnvironmentVariable(Constants.TEAMCITY_AGENT_GIT_VERSION, version.toString());
+          parameters.put(jetbrains.buildServer.agent.Constants.ENV_PREFIX + Constants.TEAMCITY_AGENT_GIT_VERSION, version.toString());
           if (version.isSupported()) {
             LOG.info("Detected git at " + path);
-            setPathToGit(agent, path);
+            setPathToGit(parameters, path);
             break;
           } else {
             LOG.debug("TeamCity supports Git version " + GitVersion.MIN + " or higher, git at " + path + " has version " + version + " and will not be used");
@@ -75,27 +80,28 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
       LOG.debug("Path to git configured: " + configuredGitPath + ", will not try to detect git");
       try {
         final GitVersion version = new AgentGitFacadeImpl(configuredGitPath).version().call();
-        agent.getConfiguration().addEnvironmentVariable(Constants.TEAMCITY_AGENT_GIT_VERSION, version.toString());
+        parameters.put(jetbrains.buildServer.agent.Constants.ENV_PREFIX + Constants.TEAMCITY_AGENT_GIT_VERSION, version.toString());
       } catch (VcsException e) {
         LOG.debug("Cannot run git at " + configuredGitPath, e);
       }
     }
-    detectGitLfs(agent);
-    detectSSH(agent);
-  }
+    detectGitLfs(parameters);
+    detectSSH(parameters);
 
+    return parameters;
+  }
   @Nullable
-  private String getConfiguredGitPath(@NotNull BuildAgent agent) {
-    Map<String, String> envVars = getEnvironmentVariables(agent);
+  private String getConfiguredGitPath() {
+    Map<String, String> envVars = getBuildEnvironmentVariables();
     return envVars.get(Constants.TEAMCITY_AGENT_GIT_PATH);
   }
 
-  private void setPathToGit(@NotNull BuildAgent agent, String path) {
-    agent.getConfiguration().addEnvironmentVariable(Constants.TEAMCITY_AGENT_GIT_PATH, path);
+  private void setPathToGit(@NotNull Map<String, String> parameters, String path) {
+    parameters.put(jetbrains.buildServer.agent.Constants.ENV_PREFIX + Constants.TEAMCITY_AGENT_GIT_PATH, path);
   }
 
-  private Map<String, String> getEnvironmentVariables(@NotNull BuildAgent agent) {
-    return agent.getConfiguration().getBuildParameters().getEnvironmentVariables();
+  private Map<String, String> getBuildEnvironmentVariables() {
+    return myBuildAgentConfiguration.getBuildParameters().getEnvironmentVariables();
   }
 
   private List<String> getCandidatePaths() {
@@ -119,7 +125,7 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
     return SystemInfo.isWindows ? WIN_PATHS : UNIX_PATHS;
   }
 
-  private void detectGitLfs(@NotNull BuildAgent agent) {
+  private void detectGitLfs(@NotNull Map<String, String> parameters) {
     try {
       GeneralCommandLine cmd = new GeneralCommandLine();
       cmd.setPassParentEnvs(true);
@@ -134,7 +140,7 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
           int idx = line.indexOf(" ");
           if (idx > 0) {
             String version = line.substring(GIT_LFS_VERSION_PREFIX.length(), idx);
-            agent.getConfiguration().addConfigurationParameter("teamcity.gitLfs.version", version);
+            parameters.put("teamcity.gitLfs.version", version);
             break;
           }
         }
@@ -144,7 +150,7 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
     }
   }
 
-  private void detectSSH(@NotNull BuildAgent agent) {
+  private void detectSSH(@NotNull Map<String, String> parameters) {
     try {
       final GeneralCommandLine cmd = new GeneralCommandLine();
       cmd.setExePath("ssh");
@@ -155,7 +161,7 @@ public class AgentStartupGitDetector extends AgentLifeCycleAdapter {
         final String line = result.getStderr();
         if (line.startsWith("OpenSSH_") && line.contains(",")) {
           final String version = line.substring(0, line.indexOf(",") - 1);
-          agent.getConfiguration().addConfigurationParameter("teamcity.git.ssh.version", version);
+          parameters.put("teamcity.git.ssh.version", version);
         }
       }
     } catch (Throwable t) {
--- git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/AgentStartupGitDetectorTest.java
+++ git-tests/src/jetbrains/buildServer/buildTriggers/vcs/git/tests/AgentStartupGitDetectorTest.java
@@ -16,20 +16,18 @@
 
 package jetbrains.buildServer.buildTriggers.vcs.git.tests;
 
-import jetbrains.buildServer.agent.AgentLifeCycleListener;
-import jetbrains.buildServer.agent.BuildAgent;
+import java.util.HashMap;
+import java.util.Map;
+import jetbrains.buildServer.ExtensionHolder;
 import jetbrains.buildServer.agent.BuildAgentConfiguration;
 import jetbrains.buildServer.agent.BuildParametersMap;
+import jetbrains.buildServer.agent.config.AgentParametersSupplier;
 import jetbrains.buildServer.buildTriggers.vcs.git.Constants;
 import jetbrains.buildServer.buildTriggers.vcs.git.agent.AgentStartupGitDetector;
-import jetbrains.buildServer.util.EventDispatcher;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author dmitry.neverov
@@ -38,40 +36,41 @@ import java.util.Map;
 public class AgentStartupGitDetectorTest {
   private Mockery myContext;
   private AgentStartupGitDetector myDetector;
+  private BuildAgentConfiguration config;
+
 
   @BeforeMethod
   public void setUp() {
     myContext = new Mockery();
-    myDetector = new AgentStartupGitDetector(EventDispatcher.create(AgentLifeCycleListener.class));
+    config = myContext.mock(BuildAgentConfiguration.class);
+    final ExtensionHolder extensionHolder = myContext.mock(ExtensionHolder.class);
+    myContext.checking(new Expectations(){{
+      exactly(1).of(extensionHolder).registerExtension(with(AgentParametersSupplier.class), with(AgentStartupGitDetector.class.getName()), with(aNonNull(AgentStartupGitDetector.class)));
+    }});
+    myDetector = new AgentStartupGitDetector(extensionHolder, config);
   }
 
   public void should_do_nothing_if_path_to_git_set_by_user() {
-    final BuildAgent agent = myContext.mock(BuildAgent.class);
     myContext.checking(new Expectations() {{
-      BuildAgentConfiguration config = myContext.mock(BuildAgentConfiguration.class);
       BuildParametersMap paramsMap = myContext.mock(BuildParametersMap.class);
       Map<String, String> params = new HashMap<String, String>() {{put(Constants.TEAMCITY_AGENT_GIT_PATH, "/some/path/to/git");}};
-      atLeast(1).of(agent).getConfiguration(); will(returnValue(config));
       atLeast(1).of(config).getBuildParameters(); will(returnValue(paramsMap));
       atLeast(1).of(paramsMap).getEnvironmentVariables(); will(returnValue(params));
     }});
-    myDetector.afterAgentConfigurationLoaded(agent);
+    myDetector.getParameters();
 
     myContext.assertIsSatisfied();
   }
 
   private void should_set_path_to_detected_git_if_not_set_by_user() {
-    final BuildAgent agent = myContext.mock(BuildAgent.class);
     myContext.checking(new Expectations() {{
-      BuildAgentConfiguration config = myContext.mock(BuildAgentConfiguration.class);
       BuildParametersMap paramsMap = myContext.mock(BuildParametersMap.class);
       Map<String, String> params = new HashMap<String, String>();
-      atLeast(1).of(agent).getConfiguration(); will(returnValue(config));
       atLeast(1).of(config).getBuildParameters(); will(returnValue(paramsMap));
       atLeast(1).of(paramsMap).getEnvironmentVariables(); will(returnValue(params));
       atLeast(1).of(config).addEnvironmentVariable(with(Constants.TEAMCITY_AGENT_GIT_PATH), with(any(String.class)));
     }});
-    myDetector.afterAgentConfigurationLoaded(agent);
+    myDetector.getParameters();
 
     myContext.assertIsSatisfied();
   }
