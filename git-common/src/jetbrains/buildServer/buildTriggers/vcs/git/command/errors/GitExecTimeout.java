63112af168fea514264e172663abfdf9ddb4cd7f
Pavel Sher <pavel.sher@jetbrains.com>
2023-06-02 13:06:53 +0200

    add a bit more details to the error message

--- git-common/src/jetbrains/buildServer/buildTriggers/vcs/git/command/errors/GitExecTimeout.java
+++ git-common/src/jetbrains/buildServer/buildTriggers/vcs/git/command/errors/GitExecTimeout.java
@@ -17,13 +17,14 @@
 package jetbrains.buildServer.buildTriggers.vcs.git.command.errors;
 
 import jetbrains.buildServer.vcs.VcsException;
+import org.jetbrains.annotations.NotNull;
 
 public class GitExecTimeout extends VcsException {
   public GitExecTimeout() {
     super("Timeout exception");
   }
 
-  public GitExecTimeout(Throwable cause) {
-    super("Timeout exception", cause);
+  public GitExecTimeout(@NotNull Throwable cause) {
+    super("Timeout exception: " + cause.toString(), cause);
   }
 }
