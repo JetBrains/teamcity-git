56ecfcecdac084e5979a8d406e410136471184f9
Ilya Voronin <ilya.voronin@jetbrains.com>
2023-08-29 10:17:43 +0000

    TW-74406 add proxy support to SSLInvestigator Merge-request: TC-MR-6563 Merged-by: Ilya Voronin <ilya.voronin@jetbrains.com>

--- git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/ssl/SSLInvestigator.java
+++ git-agent/src/jetbrains/buildServer/buildTriggers/vcs/git/agent/ssl/SSLInvestigator.java
@@ -26,7 +26,6 @@ import jetbrains.buildServer.buildTriggers.vcs.git.agent.AgentGitFacade;
 import jetbrains.buildServer.buildTriggers.vcs.git.command.ssl.SslOperations;
 import jetbrains.buildServer.serverSide.TeamCityProperties;
 import jetbrains.buildServer.util.FileUtil;
-import jetbrains.buildServer.util.StringUtil;
 import jetbrains.buildServer.util.ssl.TrustStoreIO;
 import org.apache.commons.codec.CharEncoding;
 import org.apache.log4j.Logger;
@@ -212,12 +211,13 @@ public class SSLInvestigator {
   public static class SSLCheckerImpl implements SSLChecker {
     @Override
     public boolean canConnect(@NotNull final SSLContext sslContext, @NotNull final String host, final int port) throws Exception {
-      final SSLSocket socket = (SSLSocket)sslContext.getSocketFactory().createSocket(host, port);
-      socket.setSoTimeout(TeamCityProperties.getInteger("teamcity.ssl.checkTimeout.git", 10 * 1000));
       try {
-        socket.startHandshake();
-        socket.close();
-      } catch (Exception e) {
+        SSLConnectionCheck.tryConnect(sslContext, host, port);
+      }
+      catch (SSLConnectionCheck.UnexpectedErrorException e) {
+        throw e;
+      }
+      catch (Exception e) {
         /* can't connect with custom certificate */
         return false;
       }
